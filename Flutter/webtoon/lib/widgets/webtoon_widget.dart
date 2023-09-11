3229a909ca2b92bda786286442ecfc3c03e31dca
daehee__Archeive <lavitaebella337@gmail.com>
2023-09-11 23:13:16 +0900

    files updated

--- Flutter/webtoon/lib/widgets/webtoon_widget.dart
+++ Flutter/webtoon/lib/widgets/webtoon_widget.dart
@@ -27,42 +27,43 @@ class Webtoon extends StatelessWidget {
           ),
         );
       },
-      child: Column(
-        children: [
-          Hero(
-            tag: id,
-            child: Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 11),
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ],
-              ),
-              child: Image.network(
-                thumb,
-                headers: const {
-                  "User-Agent":
-                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Column(
+          children: [
+            Hero(
+              tag: id,
+              child: Container(
+                width: 250,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 5,
+                      offset: const Offset(5, 6),
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ],
+                ),
+                child: Image.network(
+                  thumb,
+                  headers: const {
+                    "User-Agent":
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                  },
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 22,
+            const SizedBox(
+              height: 20,
             ),
-          ),
-        ],
+            Text(
+              title,
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w300),
+            ),
+          ],
+        ),
       ),
     );
   }
